eb140b7e30dceebbbe5b2d3eac249470e032a663
LEANDRO DOS SANTOS ARAUJO <leandro.araujo@alunos.sc.senac.br>
2025-04-11 21:00:40 -0300

    Adicionando exercicio 03

---
+++ ExerciciosEntra21/src/Exercicio03.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio03 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+
+    int numero = 4;
+    for (int i = 0; i <= 10; i++) {
+        System.out.println(numero + "x" + i + "=" + (numero * i));
+
+
+    }
+
+
+
+        }
+    }

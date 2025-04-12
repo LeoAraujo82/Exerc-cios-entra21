d74d74b1f9ecf7f92e3e025a6549225ccee714de
LEANDRO DOS SANTOS ARAUJO <leandro.araujo@alunos.sc.senac.br>
2025-04-11 21:14:14 -0300

    Adicionando exercicio 04

---
+++ ExerciciosEntra21/src/Exercicio04.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio04 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+
+    int N = 20;
+
+        System.out.println("Números de 0 até " + N + ":");
+
+    for (int i = 0; i <= N; i++) {
+    if (i % 2 == 0)  {
+        System.out.println(i + " é par");
+    } else {
+        System.out.println(i + "é impar");
+
+    }
+
+    }
+
+
+
+        }
+    }

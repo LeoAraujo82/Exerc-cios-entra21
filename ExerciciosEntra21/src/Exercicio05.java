2b92f7965289a00c4a2af1c5056383f3e394aee1
LEANDRO DOS SANTOS ARAUJO <leandro.araujo@alunos.sc.senac.br>
2025-04-11 21:34:42 -0300

    Adicionando exercicio 05

---
+++ ExerciciosEntra21/src/Exercicio05.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio05 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+
+        int N = 15;
+        int i = 1;
+
+        System.out.println("Números impares de 1 até " + N + ":");
+
+        while (i <= N) {
+            System.out.println(i);
+            i += 2;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
